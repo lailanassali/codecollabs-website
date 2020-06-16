dd6ab0448a3d4c6f385b5254568a8430a3403453
Benjamin <benjamin@eulercoder.com>
2020-06-16 20:25:18 +0100

    update the logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,19 @@
-var menuIcon = document.querySelector('.hamburger-menu');
-var line1 = document.querySelector('.line-1');
-var line2 = document.querySelector('.line-2');
-var line3 = document.querySelector('.line-3');
-var navList = document.querySelector('.nav-list');
-menuIcon.addEventListener('click', () => {
+var menuIcon = document.querySelector(".navbar");
+var hamburger1 = document.querySelector(".hamburger-menu");
+var hamburger2 = document.querySelector(".hamburger-menu2");
+
+var navList = document.querySelector(".nav-list");
+menuIcon.addEventListener("click", () => {
   // navbar.classList.toggle('change');
-    console.log('hey');
 
-    if (navList.style.display === 'block') {
-        navList.style.display = 'none';
-        line1.style.backgroundColor = 'white';
-        line2.style.backgroundColor = 'white';
-        line3.style.backgroundColor = 'white';
-    } else {
-        line1.style.background = 'black';
-        line2.style.background = 'black';
-        line3.style.background = 'black';
-        navList.style.display = 'block';
-    }
+  if (navList.style.display === "block") {
+    navList.style.display = "none";
 
-   
+    hamburger1.style.display = "flex";
+    hamburger2.style.display = "none";
+  } else {
+    hamburger2.style.display = "flex";
+    hamburger1.style.display = "none";
+    navList.style.display = "block";
+  }
 });
