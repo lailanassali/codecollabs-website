ba99df0bb8b3c7654e0bd9a56c1e345d3f2395f9
Laila <lailapyt@yahoo.com>
2020-06-18 15:46:31 +0100

    add responsiveness in images across devices

--- src/js/custom.js
+++ src/js/custom.js
@@ -8,8 +8,17 @@
                 responsive: [{
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: false
+                        }
+                    },
+                             {
+                        breakpoint: 800,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
                             infinite: true,
                             dots: false
                         }
